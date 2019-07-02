20a20bbe8ad1013fad231b76c109a37dce8c49ba
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-02 16:19:41 -0400

    get github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const {
     check,
     validationResult
 } = require('express-validator');
+const request = require('request');
+const config = require('config');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -313,4 +315,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 });
 
 
+// @route  GET api/profile/github/:username
+// @desc   Get github user repos
+// @access Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: {
+                'user-agent': 'node.js'
+            }
+
+        };
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                return res.status(404).json({
+                    msg: 'No github profile found'
+                });
+            }
+            res.json(JSON.parse(body));
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
