7853db5198bf9f894af9f26cebe3e941e19437d4
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-24 11:30:13 -0400

    react router set up for login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
 import Navbar from './Components/Layout/Navbar'
 import Landing from './Components/Layout/Landing'
+import Register from './Components/Auth/Register'
+import Login from './Components/Auth/Login'
 
 import './App.css';
 
 const App = () => 
+  <Router>
   <Fragment>
-    <Navbar />
-    <Landing />
-</Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+  </Fragment>
+  </Router>
 export default App;
