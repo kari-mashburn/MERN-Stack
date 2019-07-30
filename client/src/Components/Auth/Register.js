9177d6116f5b6065ecbcdc4a2a6e0bc35300e946
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-30 11:21:48 -0400

    setting up the authentication reducer and getting register redux to work

--- client/src/Components/Auth/Register.js
+++ client/src/Components/Auth/Register.js
@@ -2,10 +2,11 @@ import React, { Fragment, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
 import PropTypes from 'prop-types'
 
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState( {
         name: '',
         email: '',
@@ -38,7 +39,8 @@ const Register = ({setAlert}) => {
             //         console.error(err.response.data)   
             //     }
             // }
-            console.log( 'Success!' )
+            // console.log( 'Success!' )
+            register( { name, email, password } );
         }
     }
     return (
@@ -47,10 +49,10 @@ const Register = ({setAlert}) => {
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={e =>onSubmit(e)}>
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" value={name} onChange={e=>onChange(e)} required />
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e=>onChange(e)}  />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)} required />
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)}  />
                     <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
             <div className="form-group">
@@ -81,5 +83,6 @@ const Register = ({setAlert}) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
-export default connect(null, {setAlert})(Register)
+export default connect(null, {setAlert, register})(Register)
