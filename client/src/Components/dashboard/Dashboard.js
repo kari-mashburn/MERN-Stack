765301a6f01ae87dae95872d4ccbd5aa7c2f2a30
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-30 14:37:42 -0400

    create profile created and working properly with the database

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import { getCurrentProfile } from '../../actions/profile'
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}}) => {
     useEffect( () => {
         getCurrentProfile();
-    }, [] );
+    }, [ getCurrentProfile ] );
     return loading && profile === null ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Dashboard</h1>
         <p className="lead"><i className="fas fa-user"></i>Welcome {user && user.name}</p>
--- client/src/Components/profile-forms/CreateProfile.js
+++ client/src/Components/profile-forms/CreateProfile.js
@@ -1,8 +1,11 @@
-import React, {useState, Fragment} from 'react'
+import React, { useState, Fragment } from 'react'
+import {Link, withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+
+const CreateProfile = ({createProfile, history}) => {
     const [formData, setFormData] = useState( {
         company: '',
         website: '',
@@ -22,13 +25,18 @@ const CreateProfile = props => {
 
     const { company, website, location, status, skills, githubusername, bio, twitter, facebook, linkedin, youtube, instagram } = formData;
 
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+    const onChange = e => setFormData( { ...formData, [e.target.name]: e.target.value } );
+
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile( formData, history );
+    }
     return (
     <Fragment>
       <h1 className="large text-primary">Create Your Profile</h1>
       <p className="lead"><i className="fas fa-user"></i> Let's get some information to make your profile stand out</p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e=> onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -114,9 +122,6 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
-}
-const mapStateToProps = state => ( {
-    
-})
-export default connect()(CreateProfile)
+    createProfile: PropTypes.func.isRequired,
+} 
+export default connect(null, {createProfile})(withRouter(CreateProfile))
