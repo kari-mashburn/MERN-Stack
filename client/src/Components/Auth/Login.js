27dab1e4141345a40960e2122ef4318f5ca4d2c5
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-30 12:52:18 -0400

    got login working with redux

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,7 +1,10 @@
-import React, { Fragment, useState } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Fragment, useState, } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import {login} from '../../actions/auth'
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData] = useState( {
         email: '',
         password: '',
@@ -10,9 +13,14 @@ const Login = () => {
     const onChange = e => setFormData( { ...formData, [e.target.name]: e.target.value } );
     const onSubmit = async e => {
         e.preventDefault();
-        console.log( 'Success!' )
+        login( email, password );
     };
     
+    //Redirect if logged in
+    if ( isAuthenticated ) {
+        return <Redirect to="/dashboard" />
+    }
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign In</h1>
@@ -36,4 +44,13 @@ const Login = () => {
         </Fragment>
     )
 }
-export default Login
+
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
+const mapStateToProps = state => ( {
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {login})(Login)
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import {setAlert} from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types'
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR , LOGIN_SUCCESS, LOGIN_FAIL} from './types'
 import setAuthToken from '../utils/setAuthToken'
 // Load User
 export const loadUser = () => async dispatch => {
@@ -35,6 +35,7 @@ export const register = ( { name, email, password } ) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         } );
+         dispatch( loadUser() );
     } catch ( err ) {
         const errors = err.response.data.errors;
         if ( errors ) {
@@ -42,6 +43,39 @@ export const register = ( { name, email, password } ) => async dispatch => {
         }
         dispatch( {
             type: REGISTER_FAIL
+        } );
+    }
+}
+
+//Login user
+export const login = ( 
+    email,
+    password
+ ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    const body = JSON.stringify( {
+        email,
+        password
+    } );
+
+    try {
+        const res = await axios.post( '/api/auth', body, config );
+        dispatch( {
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        } );
+        dispatch( loadUser() );
+    } catch ( err ) {
+        const errors = err.response.data.errors;
+        if ( errors ) {
+            errors.forEach( error => dispatch( setAlert( error.msg, 'danger' ) ) );
+        }
+        dispatch( {
+            type: LOGIN_FAIL
         } );
     }
 }
