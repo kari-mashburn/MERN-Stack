1dacd70f659adc5b077ed67811b9a8cbe7b60e29
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-31 14:42:07 -0400

    set up redux for getting all repos, get profile by id, and getting all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { setAlert } from './alert'
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED } from './types'
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED, GET_PROFILES, GET_REPOS } from './types'
 
 // Get the current users' profile
 export const getCurrentProfile = () => async dispatch => {
@@ -15,6 +15,64 @@ export const getCurrentProfile = () => async dispatch => {
         dispatch( {
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
+        } );
+    }
+};
+
+//Get all Profiles
+export const getProfiles = () => async dispatch => {
+    dispatch( { type: CLEAR_PROFILE } );
+    try {
+        const res = await axios.get( '/api/profile' );
+        dispatch( {
+            type: GET_PROFILES,
+            payload: res.data
+        } );
+    } catch ( err ) {
+        dispatch( {
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        } );
+    }
+};
+
+//Get Profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get( `/api/profile/user/${userId}`);
+        dispatch( {
+            type: GET_PROFILE,
+            payload: res.data
+        } );
+    } catch ( err ) {
+        dispatch( {
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        } );
+    }
+};
+
+//Get GitHub Repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+        dispatch( {
+            type: GET_REPOS,
+            payload: res.data
+        } );
+    } catch ( err ) {
+        dispatch( {
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
         } );
     }
 };
