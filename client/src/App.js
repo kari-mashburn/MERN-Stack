c80a6f1d51c98ec4463c2039995ce4aa74963ca4
Kari Mashburn <kmashburn@fullsail.edu>
2019-07-31 13:55:31 -0400

    addEducation and addExperience working
    fixed error from yesterday

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Dashboard from './Components/dashboard/Dashboard'
 import CreateProfile from './Components/profile-forms/CreateProfile'
 import PrivateRoute from './Components/routing/PrivateRoute'
 import EditProfile from './Components/profile-forms/EditProfile'
+import AddExperience from './Components/profile-forms/AddExperience'
+import AddEducation from './Components/profile-forms/AddEducation'
 //Redux
 import { Provider } from 'react-redux'
 import store from './store'
@@ -36,8 +38,10 @@ const App = () => {
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
             </section>
         </Fragment>
