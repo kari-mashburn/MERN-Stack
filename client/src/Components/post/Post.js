b6abeab78c6cf11e17d07857d822ac41807a89a9
Kari Mashburn <kmashburn@fullsail.edu>
2019-08-01 10:53:54 -0400

    delete comment working

--- client/src/Components/post/Post.js
+++ client/src/Components/post/Post.js
@@ -6,6 +6,7 @@ import Spinner from '../Layout/Spinner'
 import { getPost } from '../../actions/post'
 import PostItem from '../posts/PostItem'
 import CommentForm from '../post/CommentForm'
+import CommentItem from '../post/CommentItem'
 
 const Post = ( { getPost, post: { post, loading }, match } ) => {
     useEffect(() => {
@@ -15,6 +16,11 @@ const Post = ( { getPost, post: { post, loading }, match } ) => {
         <Link to="/posts" className="btn">Back to Posts</Link>
         <PostItem post={post} showActions={false} />
         <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map( comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 }
 
