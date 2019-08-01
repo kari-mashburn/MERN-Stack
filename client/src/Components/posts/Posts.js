e7ec996919f6351af02ba7442413bc69dca5e7cb
Kari Mashburn <kmashburn@fullsail.edu>
2019-08-01 09:42:26 -0400

    got posts to show up on page with comment and like count

--- client/src/Components/posts/Posts.js
+++ client/src/Components/posts/Posts.js
@@ -4,14 +4,24 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import Spinner from '../Layout/Spinner'
+import PostItem from './PostItem'
 
 const Posts = ( { getPosts, post: { posts, loading } } ) => {
     useEffect( () => {
         getPosts();
     }, [getPosts] );
-    return (
+    return loading ? <Spinner /> : (
         <Fragment>
-            
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Welcome to the community
+            </p>
+            {/* Post Form */}
+            <div className='posts'>
+                {posts.map(post => (
+                <PostItem key={post._id} post={post} />
+                ))}
+            </div>
         </Fragment>
     )
 }
